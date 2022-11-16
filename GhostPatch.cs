200d05cf4fe76c9b3442226ebee590ddd6b66d61
jasonleh99 <jasonleh99@gmail.com>
2022-11-15 22:28:11 -0800

    Source upload

---
+++ GhostPatch.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx.Logging;
+using Kitchen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PlateupPrepGhost
+{
+    class GhostPatch
+    {
+        private const string PLAYERS_COLLISION_LAYER_NAME = "Players";
+        private const string DYNAMIC_OBJECT_COLLISION_LAYER_NAME = "Default";
+        private const string STATIC_OBJECT_COLLISION_LAYER_NAME = "Statics";
+
+        private static readonly ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource("GhostPatch");
+        private static bool GhostModeActivated = false;
+
+        public static bool Update_CheckPrepState(PlayerView __instance, Rigidbody ___Rigidbody)
+        {
+            if (GameInfo.IsPreparationTime && !GhostModeActivated)
+            {
+                logger.LogInfo("Activating Ghost Mode");
+                SetGhostMode(true);
+                GhostModeActivated = true;
+            } else if (!GameInfo.IsPreparationTime && GhostModeActivated)
+            {
+                logger.LogInfo("Deactivating Ghost Mode");
+                SetGhostMode(false);
+                GhostModeActivated = false;
+            }
+            
+            return true;
+        }
+
+        public static void SetGhostMode(bool enable)
+        {
+            Physics.IgnoreLayerCollision(
+                LayerMask.NameToLayer(PLAYERS_COLLISION_LAYER_NAME)
+                , LayerMask.NameToLayer(DYNAMIC_OBJECT_COLLISION_LAYER_NAME)
+                , enable);
+            Physics.IgnoreLayerCollision(
+                LayerMask.NameToLayer(PLAYERS_COLLISION_LAYER_NAME)
+                , LayerMask.NameToLayer(STATIC_OBJECT_COLLISION_LAYER_NAME)
+                , enable);
+        }
+    }
+}
+/***
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
---
+++ PlateupPrepGhost.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Kitchen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlateupPrepGhost
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class PlateupPrepGhost : BaseUnityPlugin
+    {
+        public const string pluginGuid = "happening.plateup.plateupprepghost";
+        public const string pluginName = "Plateup Prep Ghost";
+        public const string pluginVersion = "1.0";
+
+        public void Awake()
+        {
+            Harmony harmony = new Harmony(pluginGuid);
+
+            harmony.Patch(AccessTools.Method(typeof(PlayerView), "Update"), 
+                prefix:
+                new HarmonyMethod(
+                    AccessTools.Method(typeof(GhostPatch), "Update_CheckPrepState")));
+        }
+
+        
+    }
+}
